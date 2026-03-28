1b89cf339361488b2e219f7fc18b1c4b3e7290a9
Kathy <kathy.ting.cheng@gmail.com>
2026-03-28 14:36:39 +1300

    Update 03_extract_bronze_data.sql

--- scripts/03_extract_bronze_data.sql
+++ scripts/03_extract_bronze_data.sql
@@ -1,8 +1,17 @@
-USE DataWarehouse;
-GO
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS
+BEGIN
+PRINT '===========================================';
+PRINT 'Loading Bronze Layer';
+PRINT '===========================================';
 
+PRINT '-------------------------------------------';
+PRINT 'Loading CRM Tables';
+PRINT '-------------------------------------------';
+
+PRINT '>> Trancating Table: bronze.crm_cust_info';
 TRUNCATE TABLE bronze.crm_cust_info;
 
+PRINT '>> Inserting Data Info: bronze.crm_cust_info';
 BULK INSERT bronze.crm_cust_info
 FROM '/var/opt/mssql/datasets/cust_info.csv'
 WITH (
@@ -11,11 +20,10 @@ WITH (
     TABLOCK
 );
 
-SELECT * FROM bronze.crm_cust_info
-SELECT COUNT(*) FROM bronze.crm_cust_info
-
-
+PRINT '>> Trancating Table: bronze.crm_prd_info';
 TRUNCATE TABLE bronze.crm_prd_info;
+
+PRINT '>> Inserting Data Info: bronze.crm_prd_info';
 BULK INSERT bronze.crm_prd_info
 FROM '/var/opt/mssql/datasets/prd_info.csv'
 WITH (
@@ -24,12 +32,10 @@ WITH (
     TABLOCK
 );
 
-SELECT * FROM bronze.crm_prd_info
-SELECT COUNT(*) FROM bronze.crm_prd_info
-
-
-
+PRINT '>> Trancating Table: bronze.crm_sales_details';
 TRUNCATE TABLE bronze.crm_sales_details;
+
+PRINT '>> Inserting Data Info: bronze.crm_sales_details';
 BULK INSERT bronze.crm_sales_details
 FROM '/var/opt/mssql/datasets/sales_details.csv'
 WITH (
@@ -38,11 +44,14 @@ WITH (
     TABLOCK
 );
 
-SELECT * FROM bronze.crm_sales_details
-SELECT COUNT(*) FROM bronze.crm_sales_details
+PRINT '-------------------------------------------';
+PRINT 'Loading ERP Tables';
+PRINT '-------------------------------------------';
 
-
+PRINT '>> Trancating Table: bronze.erp_cust_az12';
 TRUNCATE TABLE bronze.erp_cust_az12;
+
+PRINT '>> Inserting Data Info: bronze.erp_cust_az12';
 BULK INSERT bronze.erp_cust_az12
 FROM '/var/opt/mssql/datasets/CUST_AZ12.csv'
 WITH (
@@ -51,12 +60,10 @@ WITH (
     TABLOCK
 );
 
-SELECT * FROM bronze.erp_cust_az12
-SELECT COUNT(*) FROM bronze.erp_cust_az12
-
-
-
+PRINT '>> Trancating Table: bronze.erp_loc_a101';
 TRUNCATE TABLE bronze.erp_loc_a101;
+
+PRINT '>> Inserting Data Info: bronze.erp_loc_a101';
 BULK INSERT bronze.erp_loc_a101
 FROM '/var/opt/mssql/datasets/LOC_A101.csv'
 WITH (
@@ -65,12 +72,10 @@ WITH (
     TABLOCK
 );
 
-SELECT * FROM bronze.erp_loc_a101
-SELECT COUNT(*) FROM bronze.erp_loc_a101
-
-
-
+PRINT '>> Trancating Table: bronze.erp_px_cat_g1v2';
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+PRINT '>> Inserting Data Info: bronze.erp_px_cat_g1v2';
 BULK INSERT bronze.erp_px_cat_g1v2
 FROM '/var/opt/mssql/datasets/PX_CAT_G1V2.csv'
 WITH (
@@ -78,6 +83,4 @@ WITH (
     FIELDTERMINATOR = ',',
     TABLOCK
 );
-
-SELECT * FROM bronze.erp_px_cat_g1v2
-SELECT COUNT(*) FROM bronze.erp_px_cat_g1v2
+END
