3f9c1bb0c0daaa65f03d8843e98f7ca199343209
Kathy <kathy.ting.cheng@gmail.com>
2026-04-04 11:45:12 +1300

    Update 03_transform_silver_crm.sql
    Add silver.crm_prd_info Transformation

--- scripts/silver/03_transform_silver_crm.sql
+++ scripts/silver/03_transform_silver_crm.sql
@@ -1,8 +1,10 @@
---Silver Layer ETL: CRM Customer Information Transformation
+--Silver Layer ETL: CRM Transformation
 --This script automates the Cleaning, Standardization, and Deduplication of CRM customer data as it moves from the Bronze (Raw) layer to the Silver (Refined) layer.
 
 USE DataWarehouse
 GO
+
+-- Customer Information Table Transformation
 
 INSERT INTO silver.crm_cust_info (
     cst_id,
@@ -33,5 +35,48 @@ FROM (
     FROM bronze.crm_cust_info
     WHERE cst_id IS NOT NULL
 )t WHERE flag_last = 1
+
+
+-- silver.crm_prd_info Table Transformation
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info; 
+CREATE TABLE silver.crm_prd_info(
+    prd_id INT,
+    cat_id NVARCHAR(50),
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
+);
+
+INSERT INTO silver.crm_prd_info (
+    prd_id,
+    cat_id,
+    prd_key,
+    prd_nm,
+    prd_cost,
+    prd_line,
+    prd_start_dt,
+    prd_end_dt
+)
+SELECT
+prd_id,
+REPLACE(SUBSTRING(prd_key, 1, 5), '-','_') AS cat_id,
+SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+prd_nm,
+ISNULL(prd_cost,0) AS prd_cost,
+CASE UPPER(TRIM(prd_line))
+     WHEN 'M' THEN 'Mountain'
+     WHEN 'R' THEN 'Road'
+     WHEN 'S' THEN 'Other Sales'
+     WHEN 'T' THEN 'Touring'
+     ELSE 'n/a'
+END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt ,
+CAST (LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+FROM bronze.crm_prd_info
 
 
