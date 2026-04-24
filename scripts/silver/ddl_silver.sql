6647f1fe207e29d957696e96fdc07e541765b7e1
Kathy <kathy.ting.cheng@gmail.com>
2026-04-24 13:16:29 +1200

    Create ddl_silver.sql

---
+++ scripts/silver/ddl_silver.sql
@@ -0,0 +1,10 @@
+/*
+====================================================================================================
+DDL Script: Create Silver Tables:
+====================================================================================================
+Script Purpose:
+  This script creates tables in the silver schema, dropping existing tables if they already exist.
+  Run this script to re-define the DDL structure of bronze table
+====================================================================================================
+*/
+
